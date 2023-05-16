3438fd5cc374e6493f93f392210a9ef071ec3494
Jonathan Lee <jonathanlee882@gmail.com>
2023-05-16 18:02:17 +1000

    created two components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Todo from "./components/Todo.jsx"
+import Title from "./components/Titie"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title></Title>
+      <h1>My Todo List</h1>
+
+      <ul>
+        <Todo></Todo>
+        <Todo></Todo>
+        <Todo></Todo>
+      </ul>
     </div>
   );
 }
